fe9c2cbb1c63ba7b70db7121bc4c9a2c03901e35
Dimas Abreu Archanjo Dutra <dimas.dutra@mail.wvu.edu>
2026-06-23 16:52:00 -0400

    Updated launch file by inspecting franka_fr3_moveit_config.

--- src/fr3_moveit_servo/launch/fr3_moveit_servo.launch.py
+++ src/fr3_moveit_servo/launch/fr3_moveit_servo.launch.py
@@ -5,31 +5,42 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import Command, FindExecutable, LaunchConfiguration
 from launch_ros.actions import Node
 from moveit_configs_utils import MoveItConfigsBuilder
 
+
 def generate_launch_description():
-    # 1. Declare runtime arguments
+    # Runtime arguments
     robot_ip_arg = DeclareLaunchArgument("robot_ip", default_value="172.16.0.2")
     use_fake_arg = DeclareLaunchArgument("use_fake_hardware", default_value="false")
 
-    # 2. Include the official Franka MoveIt launch sequence
-    franka_moveit_dir = get_package_share_directory("franka_moveit_config")
+    # Include the official Franka MoveIt launch sequence
+    franka_moveit_dir = get_package_share_directory("franka_fr3_moveit_config")
     base_moveit_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(os.path.join(franka_moveit_dir, "launch", "moveit.launch.py")),
+        PythonLaunchDescriptionSource(
+            os.path.join(franka_moveit_dir, "launch", "moveit.launch.py")
+        ),
         launch_arguments={
             "robot_ip": LaunchConfiguration("robot_ip"),
             "use_fake_hardware": LaunchConfiguration("use_fake_hardware"),
-        }.items()
+        }.items(),
+    )
+
+    franka_xacro_file = os.path.join(
+        get_package_share_directory("franka_bringup"), "urdf", "franka_arm.urdf.xacro"
+    )
+
+    franka_kinematics_file = os.path.join(
+        get_package_share_directory("franka_fr3_moveit_config"),
+        "config/kinematics.yaml",
     )
 
     # 3. Pull configurations specifically for the Servo Node parameter mapping
     moveit_config = (
-        MoveItConfigsBuilder("franka_fr3_moveit_config", package_path="share/franka_fr3_moveit_config")
-        .robot_description()
-        .robot_description_semantic()
-        .robot_description_kinematics()
+        MoveItConfigsBuilder("franka_fr3_moveit_config")
+        .robot_description(file_path=franka_xacro_file)
+        .robot_description_kinematics(file_path=franka_kinematics_file)
         .to_moveit_configs()
     )
 
@@ -50,9 +61,6 @@ def generate_launch_description():
         ],
     )
 
-    return LaunchDescription([
-        robot_ip_arg,
-        use_fake_arg,
-        base_moveit_launch,
-        servo_node
-    ])
+    return LaunchDescription(
+        [robot_ip_arg, use_fake_arg, base_moveit_launch, servo_node]
+    )
