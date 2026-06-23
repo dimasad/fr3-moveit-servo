f9b7806264f8b929332624238e1e635105c132f9
Dimas Abreu Archanjo Dutra <dimas.dutra@mail.wvu.edu>
2026-06-23 15:41:07 -0400

    Including launch files in installation.

--- src/fr3_moveit_servo/setup.py
+++ src/fr3_moveit_servo/setup.py
@@ -1,29 +1,30 @@
+from glob import glob
+
 from setuptools import find_packages, setup
 
-package_name = 'fr3_moveit_servo'
+package_name = "fr3_moveit_servo"
 
 setup(
     name=package_name,
-    version='0.0.1',
-    packages=find_packages(exclude=['test']),
+    version="0.0.1",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        ("share/" + package_name + "/launch", glob("launch/*.launch.py")),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='root',
-    maintainer_email='root@todo.todo',
-    description='TODO: Package description',
-    license='MIT',
+    maintainer="root",
+    maintainer_email="root@todo.todo",
+    description="TODO: Package description",
+    license="MIT",
     extras_require={
-        'test': [
-            'pytest',
+        "test": [
+            "pytest",
         ],
     },
     entry_points={
-        'console_scripts': [
-        ],
+        "console_scripts": [],
     },
 )
